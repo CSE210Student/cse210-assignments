609da5caf5f7e60d4b3b5c30469e64074dc79281
WDDStudent <asgould@byui.edu>
2025-06-21 12:47:40 -0600

    Outline

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,26 @@
+using System.Runtime.CompilerServices;
+
+public class Journal
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {
+
+    }
+
+    public void DisplayAll()
+    {
+
+    }
+
+    public void SaveToFile(string File)
+    {
+
+    }
+
+    public void LoadFile(string file)
+    {
+        
+    }
+}
