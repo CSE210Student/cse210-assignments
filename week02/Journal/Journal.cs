6ea4ed55a342d59510abab6c2a5de1f102ca1acb
WDDStudent <asgould@byui.edu>
2025-06-21 16:50:21 -0600

    Loading File fix, Add Entry

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -1,12 +1,25 @@
+using System.Reflection.Metadata.Ecma335;
 using System.Runtime.CompilerServices;
 
 public class Journal
 {
-    public List<Entry> _entries;
+    public List<Entry> _entries = new List<Entry>();
 
     public void AddEntry(Entry newEntry)
     {
+        PromptGenerator prompts = new PromptGenerator();
+        Entry anEntry = new Entry();
+        anEntry._promptText = prompts.GetRandomPrompt();
+        Console.WriteLine($"Prompt: {anEntry._promptText}");
 
+        DateTime currentTime = DateTime.Now;
+        anEntry._date = currentTime.ToShortDateString();
+        Console.WriteLine($"Date: {anEntry._date}");
+
+        Console.Write(">");
+        anEntry._entryText = Console.ReadLine();
+
+        _entries.Add(anEntry);
     }
 
     public void DisplayAll()
@@ -19,17 +32,34 @@ public class Journal
 
     public void SaveToFile(string file)
     {
+        Console.WriteLine("Saving File...");
         using (StreamWriter outputFile = new StreamWriter(file))
         {
-            foreach(Entry e in _entries)
+            foreach (Entry e in _entries)
             {
-                outputFile.WriteLine(e._entryText);
+                outputFile.WriteLine($"{e._date}-{e._promptText}-{e._entryText}");
             }
         }
     }
 
     public void LoadFile(string file)
     {
+        Entry anEntry = new Entry();
+        Console.WriteLine("Loading File...");
         string[] lines = System.IO.File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("-");
+
+            string date = parts[0];
+            anEntry._date = date;
+            string prompt = parts[1];
+            anEntry._promptText = prompt;
+            string entryText = parts[2];
+            anEntry._entryText = entryText;
+
+            _entries.Add(anEntry);
+        }
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,49 +1,55 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
+using System.Xml.Serialization;
+
+//https://stackoverflow.com/questions/1753508/how-could-i-get-a-random-string-from-a-list-and-assign-it-to-a-variable
 
 class Program
 {
     static void Main(string[] args)
     {
-
+        int choice = 10;
         Journal theJournal = new Journal();
         Entry anEntry = new Entry();
-        anEntry.Display();
-
-        Console.WriteLine("Welcome to the Journal Program");
-        Console.WriteLine("Select a menu option from the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.Write("What would you like to do? ");
-        string userChoice = Console.ReadLine();
-        int choice = int.Parse(userChoice);
-
-        if (choice == 1)
+        
+        while (choice != 5)
         {
-            theJournal.AddEntry(anEntry);
-        }
+            Console.WriteLine("Welcome to the Journal Program");
+            Console.WriteLine("Select a menu option from the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            string userChoice = Console.ReadLine();
+            choice = int.Parse(userChoice);
 
-        else if (choice == 2)
-        {
-            theJournal.DisplayAll();
-        }
+            if (choice == 1)
+            {
+                theJournal.AddEntry(anEntry);
+            }
 
-        else if (choice == 3)
-        {
-            Console.Write("What is the file name? ");
-            string file = Console.ReadLine();
-            theJournal.LoadFile(file);
-        }
+            else if (choice == 2)
+            {
+                theJournal.DisplayAll();
+            }
 
-        else if (choice == 4)
-        {
-            Console.Write("What is the file name? ");
-            string file = Console.ReadLine();
-            theJournal.SaveToFile(file);
+            else if (choice == 3)
+            {
+                Console.Write("What is the file name? ");
+                string file = Console.ReadLine();
+                theJournal.LoadFile(file);
+            }
+
+            else if (choice == 4)
+            {
+                Console.Write("What is the file name? ");
+                string file = Console.ReadLine();
+                theJournal.SaveToFile(file);
+            }
         }
     }
 }
