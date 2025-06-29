814347cbc7ba141695f25d4f3471928c4c1540e0
WDDStudent <asgould@byui.edu>
2025-06-28 20:14:20 -0600

    Program additions

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
@@ -6,6 +7,24 @@ class Program
     {
         Reference r = new Reference("1 Nephi", 11, 2, 5);
         Scripture s = new Scripture(r, "And the Spirit said unto me: Behold, what desirest thou? And I said: I desire to behold the things which my father saw. And the Spirit said unto me: Believest thou that thy father saw the tree of which he hath spoken? And I said: Yea, thou knowest that I believe all the words of my father.");
+
+        string userChoice = "";
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine(s.GetDisplayText());
+            Console.WriteLine("");
+            Console.WriteLine("Press ENTER to continue or type 'quit' to exit the program.");
+            userChoice = Console.ReadLine();
+
+            if (s.IsCompletelyHidden() == true)
+            {
+                userChoice = "quit";
+            }
+
+        } while (userChoice != "quit");
 
     }
 }
