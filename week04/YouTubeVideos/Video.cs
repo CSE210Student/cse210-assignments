68a0408f46cccbfff59536e9e0a45bcd267f6a85
WDDStudent <asgould@byui.edu>
2025-07-05 13:21:24 -0600

    Video Outline

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,12 @@
+public class YouTubeVideos
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<string> _comments;
+
+    public int GetNumberOfComments()
+    {
+        return 0;
+    }
+}
