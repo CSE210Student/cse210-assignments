072969174e1a8bd6e1e919ca86f841d6b484f0b6
WDDStudent <asgould@byui.edu>
2025-07-09 20:13:58 -0600

    Week 5 Activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Samuel Bennet", "Multiiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Mary Water", "European History", "The Causes of World War II");
+        Console.WriteLine(write1.GetSummary());
+        Console.WriteLine(write1.GetWritingInfo());
     }
 }
