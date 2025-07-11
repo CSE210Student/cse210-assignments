7341dc1ca1215334d0b7fdfbda3bb278a7a799f5
WDDStudent <asgould@byui.edu>
2025-07-11 17:41:37 -0600

    Mindfulness Outline

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,28 @@
+using System.Runtime.CompilerServices;
+
+public class ListingActivity : Activity
+{
+    private int _count;
+    private List<string> _prompts;
+    private List<string> _placeholder;
+
+    public ListingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+
+    }
+
+    public List<string> GetListFromUser()
+    {
+        return _placeholder;
+    }
+}
