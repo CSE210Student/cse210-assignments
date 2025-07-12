96ee68dbc64a9ec028c7fb2086b44d4db117fbff
WDDStudent <asgould@byui.edu>
2025-07-12 03:13:09 -0600

    ShowSpinner

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,36 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<string> spinnerStrings = new List<string>();
-
-        spinnerStrings.Add("|");
-        spinnerStrings.Add("/");
-        spinnerStrings.Add("-");
-        spinnerStrings.Add("\\");
-        spinnerStrings.Add("|");
-        spinnerStrings.Add("/");
-        spinnerStrings.Add("-");
-        spinnerStrings.Add("\\");
-
-
-        DateTime startTime = DateTime.Now;
-        DateTime endTime = startTime.AddSeconds(10);
-
-        int i = 0;
-
-        while (DateTime.Now < endTime)
-        {
-            string spin = spinnerStrings[i];
-            Console.Write(spin);
-            Thread.Sleep(1000);
-            Console.Write("\b \b");
-
-            i++;
-
-            if (i >= spinnerStrings.Count)
-            {
-                i = 0;
-            }
-        }
+        
     }
 }
