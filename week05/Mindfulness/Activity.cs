38032a47a31ea49f6d4435822748b16a5b1938de
WDDStudent <asgould@byui.edu>
2025-07-12 03:09:03 -0600

    ShowSpinner

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -1,8 +1,8 @@
 public class Activity
 {
-    private string _name;
-    private string _description;
-    private int _duration;
+    protected string _name;
+    protected string _description;
+    protected int _duration;
 
     public Activity()
     {
@@ -21,7 +21,37 @@ public class Activity
 
     public void ShowSpinner(int seconds)
     {
+        List<string> spinnerStrings = new List<string>();
 
+        spinnerStrings.Add("|");
+        spinnerStrings.Add("/");
+        spinnerStrings.Add("-");
+        spinnerStrings.Add("\\");
+        spinnerStrings.Add("|");
+        spinnerStrings.Add("/");
+        spinnerStrings.Add("-");
+        spinnerStrings.Add("\\");
+
+
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(10);
+
+        int i = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            string spin = spinnerStrings[i];
+            Console.Write(spin);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+
+            i++;
+
+            if (i >= spinnerStrings.Count)
+            {
+                i = 0;
+            }
+        }
     }
 
     public void ShowCountDown(int seconds)
